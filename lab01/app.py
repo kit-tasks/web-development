a6b9e6c7a7b2161c4811fc8037f4a45a5d2c522d
Kuznetsov Maksim <maxekb@live.ru>
2023-09-21 11:49:20 +0300

    refactoring prettier

--- lab01/app.py
+++ lab01/app.py
@@ -1,4 +1,3 @@
 from flask import Flask, send_file, redirect, url_for
 
 app = Flask('lab01')
-
--- lab05/app.py
+++ lab05/app.py
@@ -5,12 +5,14 @@ app = Flask('lab05')
 
 my_items = []
 
+
 @app.route('/hello', methods=["GET"])
 def hello():
     name = request.args.get('name')
     t = datetime.now()
     time = t.strftime('%H:%M:%S')
     return render_template('hello.html', name=name, time=time)
+
 
 @app.route('/my_list', methods=["GET", "POST"])
 def my_list():
@@ -21,10 +23,13 @@ def my_list():
     else:
         return '404: NOT FOUND'
 
+
 def my_list_post():
     el = request.form['content']
-    my_items.append(el)
+    if not el in my_items:
+        my_items.append(el)
     return my_list_get()
+
 
 def my_list_get():
     return render_template('my_list.html', items=my_items)
